4b11ce4adc3bc7a07a2ed9ddfccd4b60a2d95ace
ekansh <mail.ekansh@gmail.com>
2018-01-19 15:46:41 -0500

    added getters in Category

--- src/main/java/com/example/entity/Category.java
+++ src/main/java/com/example/entity/Category.java
@@ -20,6 +20,18 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long categoryid;
 	private String name;
+	public Long getCategoryid() {
+		return categoryid;
+	}
+	public void setCategoryid(Long categoryid) {
+		this.categoryid = categoryid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	
 }
